a68dca313750611e1d56257444815a3d6244dd60
tentacode <gabriel+github@tentacode.dev>
2022-06-18 08:42:57 +0200

    :tada: Symfony init

---
+++ src/Controller/Landing/LandingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Landing;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LandingController extends AbstractController
+{
+    #[Route('/', name: 'app_landing')]
+    public function __invoke(): Response
+    {
+        return $this->render('landing/landing.html.twig', []);
+    }
+}
