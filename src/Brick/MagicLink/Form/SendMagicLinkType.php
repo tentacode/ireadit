3779a508050420d37ba10ef8e85f77cdacacd709
tentacode <gabriel+github@tentacode.dev>
2022-07-10 09:37:08 +0200

    :rotating_light: Fixing php ecs on SendMagicLinkType

--- src/Brick/MagicLink/Form/SendMagicLinkType.php
+++ src/Brick/MagicLink/Form/SendMagicLinkType.php
@@ -7,7 +7,6 @@ namespace App\Brick\MagicLink\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class SendMagicLinkType extends AbstractType
